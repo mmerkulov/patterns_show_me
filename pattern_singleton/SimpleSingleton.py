0ed965eb51a2d553763a2b0e4fced3837b348297
mikhail merkulov <m.merkulov@tinkoff.ru>
2023-04-10 16:09:41 +0300

    Добавил SingletonConnection, на примере БД проверял создание экземпляров

--- pattern_singleton/SimpleSingleton.py
+++ pattern_singleton/SimpleSingleton.py
@@ -1,26 +1,26 @@
 from abc import ABC, abstractmethod
 
 
-class AbstractSingleton(ABC):
-    _instances = None
+class AbstractSingleton(type):
+    _instances = {}
 
-    def __call__(self, *args, **kwargs):
-        if self not in self._instances:
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
             instance = super().__call__(*args, **kwargs)
-            self._instances[self] = instance
-        return self._instances[self]
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 
 
-class Singleton(AbstractSingleton):
+class Singleton(metaclass=AbstractSingleton):
     @staticmethod
     def some_code(a: int, b: int):
         print('Nice code, bro!')
         return a + b
 
 
-s1 = Singleton()
-s2 = Singleton()
+s1 = Singleton() #.some_code(100, 100)
+s2 = Singleton() #.some_code(10, 10)
 
 print(s1.some_code(100, 100))
 print(s2.some_code(10, 15))
-print(id(s1), id(s2))
+print(id(s1), id(s2), s1, s2)
---
+++ pattern_singleton/SinlgetonConnection.py
@@ -0,0 +1,43 @@
+class SingletonConnect(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            instance = super().__call__(*args, **kwargs)
+            cls._instance[cls] = instance
+        return cls._instance[cls]
+
+
+class DataBaseConnection(metaclass=SingletonConnect):
+    conn = None
+
+    def create_connect(self, connection_string):
+        if self.conn is None:
+            self.conn = psycopg2.connect(**connection_string)
+        print('Соединение открыто')
+        return self.conn
+
+    def close_connect(self):
+        self.conn.close()
+        print('Соединение закрыто')
+
+
+connection_str = {
+    "host": "localhost",
+    "port": '5432',
+    "dbname": 'test',
+    "user": 'test',
+    "password": 'test'
+}
+c1 = DataBaseConnection()
+my_conn = c1.create_connect(connection_string=connection_str)
+
+my_conn1 = c1.create_connect(connection_string=connection_str)
+print(type(c1), c1, id(c1))
+print(type(my_conn), id(my_conn))
+print(type(my_conn1), id(my_conn1))
+#
+c2 = DataBaseConnection()
+my_conn2 = c2.create_connect(connection_string=connection_str)
+print(type(c2), c2, id(c2))
+print(type(my_conn2), id(my_conn2))
